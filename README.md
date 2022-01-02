abf21323d50dd259b7e361b98cfaadf1f3cc9e87
서원석 <seows@laivdata.com>
2022-01-02 22:50:31 +0900

    프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,5 @@
+# 그림판 클론하기
+
+- 참고: https://www.youtube.com/watch?v=V4yUrRjAiDQ
+
+- 다른 점: 원래 vanillaJS로 구현하는 강의인데 타입스크립트로 구현
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>hello world</h1>
+      <h2>my name is Dog</h2>
+      <p>asdfasfdsafsdfa</p>
+    </div>
+  );
+}
+
+export default App;
